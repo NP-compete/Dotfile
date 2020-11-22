8c843d45dd16a7787542b730a44f9387f8df3fec
Soham Dutta <soham.dutta.analyst@gmail.com>
2020-11-22 15:32:53 +0000

    Added initial config

--- main.sh
+++ main.sh
@@ -1,9 +1,15 @@
-  mkdir ~/ricingStuff
+  mkdir -p ~/ricingStuff
   cd ~/ricingStuff
 
 # Remove gnome
-  sudo apt-get remove gnome* -y
-  sudo apt-get purge gnome* -y 
+  #sudo apt-get remove gnome* -y
+  #sudo apt-get purge gnome* -y 
+
+# Setup source list
+  sudo mv sources.list /etc/apt/sources.list
+  sudo apt-get update
+  sudo apt-get upgrade -y
+  sudo apt-get dist-upgrade -y
 
 # Install i3
   sudo apt-get install i3 -y
@@ -12,14 +18,28 @@
   sudo apt-get install fonts-powerline
 
 # Install i3 gaps
-  sudo apt install libxcb1-dev libxcb-keysyms1-dev  libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake
-  git clone https://www.github.com/Airblader/i3 i3-gaps
-  cd i3-gaps
-  git checkout gaps && git pull
-  autoreconf --force --install
-  rm -rf build
-  mkdir build
-  cd build
-  ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
-  make
-  sudo make install
+  ## TODO
+
+# Resolve xbacklight issue
+  Identifier='Identifier "${xrandr --verbose | grep Identifier | head -1 | awk '{ print $2 }'}"'
+  Driver='Driver "intel"'
+  Option='Option "Backlight" "${ls /sys/class/backlight}"'
+  read -r -d '' FILE_CONTENT << EOM
+Section "Device"
+    $Identifier
+    $Driver
+    $Option
+EndSection
+EOM
+  mkdir /etc/X11/xorg.conf.d/
+  echo $FILE_CONTENT > x.conf
+
+
+# Install polybar
+  sudo apt -t buster-backports install polybar
+  sudo apt-get install mpd -y
+  mkdir -p ~/.config/polybar/
+  cp /usr/share/doc/polybar/config ~/.config/polybar/
+
+  polybar example &> /dev/null 
+
